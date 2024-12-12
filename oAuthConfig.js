8003e8a6e9fca1e4127f1ee3eb5cbe4fab6d36f8
Pankaj Surti <pankajsu@microsoft.com>
2024-12-11 21:28:10 -0600

    added tenant id for teams

--- oAuthConfig.js
+++ oAuthConfig.js
@@ -1,7 +1,7 @@
 const authConfig = {
     auth: {
         clientId: 'ENTER_CLIENT_ID',
-        authority: 'https://login.microsoftonline.com/ENTER_TENANT_ID'
+        authority: 'https://login.microsoftonline.com/6e4ce203-3bc4-4838-9d68-d1c438b7ecc2'
     }
 };
  // Add here scopes for id token to be used at MS Identity Platform endpoints.
